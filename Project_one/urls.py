78ee46733f3e9a559b0a93e002af64a67d7c1702
alice5426 <71244115+alice5426@users.noreply.github.com>
2022-11-24 18:01:05 +0800

    Update urls.py

--- Project_one/urls.py
+++ Project_one/urls.py
@@ -27,12 +27,16 @@ urlpatterns = [
     path('login/', views.login),
     path('register/', views.register),
     path('login_out/', views.login_out),
+    
     path('user_center/', views.user_center),
     path('user_center/del_good/', views.del_goods),
     path('user_center/del_cart/', views.del_carts),
     path('user_center/buy_allgood/', views.buy_goods),
+    path('user_center/user_information/', views.users_information),
+    
     path('index/issue_page/', views.issue_page),
     path('index/issue_page/issue_form/', views.issue_form),
+    
     path('goods_detail_page/', views.good_detail_page),
     path('goods_detail_page/join_cart/', views.join_cart),
     re_path(r'goods_detail_page?id=(\d+)', views.good_detail_page),
